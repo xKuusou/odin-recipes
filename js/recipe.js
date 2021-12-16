ba9a0026e413d756f4778637f4074df7343c5bb8
Kuusou <xKuusou@protonmail.com>
2021-12-16 03:40:03 -0500

    Add a dark mode toggle to recipe pages

--- js/recipe.js
+++ js/recipe.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById("topBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 3 || document.documentElement.scrollTop > 3) {
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     mybutton.style.display = "block";
 } else {
     mybutton.style.display = "none";
@@ -15,4 +15,15 @@ if (document.body.scrollTop > 3 || document.documentElement.scrollTop > 3) {
 function topFunction() {
 document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
+    }
+
+function darkFunction() {
+    document.body.classList.toggle("dark-body");
+    document.getElementById("topBar").classList.toggle('dark-box');
+    document.getElementById("box2").classList.toggle("dark-box")
+    document.getElementById("box3").classList.toggle("dark-box")
+    document.getElementById("box4").classList.toggle("dark-box")
+    document.getElementById("menuBtn").classList.toggle('dark-menu');
+    document.getElementById("topBtn").classList.toggle('dark-btn');
+    document.getElementById("darkToggle").classList.toggle('dark-toggle');
     }
